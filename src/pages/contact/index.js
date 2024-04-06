573ab697c26a70374666f395cf81d4d4932a0e3c
Filipe <filipe.lima@ccc.ufcg.edu.br>
2024-04-06 18:55:00 -0300

    recaptcha is now working with correct keys

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { navigate } from "gatsby-link";
 import Layout from "../../components/Layout";
+import Recaptcha from "react-google-recaptcha";
 
 function encode(data) {
   return Object.keys(data)
@@ -21,16 +22,22 @@ export default class Index extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error));
+    if (this.state["g-recaptcha-response"]) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": form.getAttribute("name"),
+          ...this.state,
+        }),
+      })
+        .then(() => navigate(form.getAttribute("action")))
+        .catch((error) => alert(error));
+    }
+  };
+
+  handleRecaptcha = value => {
+    this.setState({ "g-recaptcha-response": value });
   };
 
   render() {
@@ -49,7 +56,6 @@ export default class Index extends React.Component {
                 data-netlify-recaptcha="true"
                 onSubmit={this.handleSubmit}
               >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                 <input type="hidden" name="form-name" value="contact" />
                 <div hidden>
                   <label>
@@ -57,7 +63,6 @@ export default class Index extends React.Component {
                     <input name="bot-field" onChange={this.handleChange} />
                   </label>
                 </div>
-                {/* <div class="g-recaptcha" data-sitekey="6Lf-irIpAAAAAGW4eNixZrYhgsMEGG-EUySjvGYE"/> */}
                 <div data-netlify-recaptcha="true"></div>
                 <div className="field">
                   <label className="label" htmlFor={"name"}>
@@ -101,7 +106,12 @@ export default class Index extends React.Component {
                       id={"message"}
                       required={true}
                     />
-                  </div>
+                  </div>          
+                  <Recaptcha
+                    ref="recaptcha"
+                    sitekey={process.env.SITE_RECAPTCHA_KEY}
+                    onChange={this.handleRecaptcha}
+                  />
                 </div>
                 <div className="field">
                   <button className="button is-link" type="submit">
